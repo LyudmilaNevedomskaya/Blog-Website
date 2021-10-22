c665a40deb1f5285362b46781a73c3227e1c71b1
Lyudmila <milo4kamoya@gmail.com>
2021-10-21 22:11:01 -0700

    Different routes added, partials separated

--- app.js
+++ app.js
@@ -12,9 +12,32 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("home", { homeStartingContent });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.composedTitle,
+    body: req.body.composedText
+  };
+  console.log(post);
+});
 
 
 
@@ -23,11 +46,6 @@ app.use(express.static("public"));
 
 
 
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
